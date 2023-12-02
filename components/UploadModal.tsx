60dd5ba0f1735ec378c13a96459fd84e125878dd
Alvin <alvinkoeinata@gmail.com>
2023-12-02 11:43:37 +0700

    first commit

---
+++ components/UploadModal.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import useUploadModal from "@/hooks/useUploadModal";
+import Modal from "./Modal";
+
+const UploadModal = () => {
+  const uploadModal = useUploadModal();
+
+  const { register, handleSubmit, reset } = useForm<FieldValues>({
+    defaultValues: {
+      author: "",
+      title: "",
+      song: null,
+      image: null,
+    },
+  });
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      // reset();
+      uploadModal.onClose();
+    }
+  };
+  return (
+    <div>
+      <Modal title="upload modal title" description="upload an mp3 file" isOpen={uploadModal.isOpen} onChange={onChange}>
+        Form
+      </Modal>
+    </div>
+  );
+};
+
+export default UploadModal;
